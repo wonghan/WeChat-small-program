43d9f41ac6b7173cd06428ba0147bcb9556ea310
wonghan <one_hund@outlook.com>
2018-09-25 00:18:10 +0800

    2.2.0

--- pages/recommend/recommend.js
+++ pages/recommend/recommend.js
@@ -1,6 +1,6 @@
 Page({
   data: {
-    url: "https://youliao.163yun.com/h5/list/?appkey=e0d49d7b17c24b08b7db6b2268984e94&channelid=14838&secretkey=1161b7ba70694683a5c32761a83e46bf&s=semi&ctag=ca198cbfa41444a4b3e9881446a6dac4&ct=only"
+    url: "https://youliao.163yun.com/h5/list/?appkey=caf4091276ca42f796002285182f76bc&secretkey=130a6f954de44e29b8d6ecf236a44eb2&s=semi&ctag=adcac61e097840cc93aeed7c341dca90&ct=only"
   },
   onShareAppMessage: function (res) {
     return {
